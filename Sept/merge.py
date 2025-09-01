ee66a59ec76cc46396b9f5a3d08f674559dd2f7a
RaulSeguraHernandez <raul3ro.segura@gmail.com>
2025-09-01 15:50:51 -0600

    Update merge.py

--- Sept/merge.py
+++ Sept/merge.py
@@ -1,2 +1,25 @@
 def merge(A):
-  
+    #verificar caso base
+    if len(A) >1:
+        mid = len(A)//2
+        L= A[:mid]
+        R= A[mid:]
+        merge(L)
+        merge(R)
+        i,j,k=0
+        while (i<len(L) and j<len(R)):
+            if L[i]<=R[j]:
+                A[k]=L[i]
+                i+=1
+            else:
+                A[k]=R[j]
+                j+=1
+            k+=1
+        while i<len(L):
+            A[k]= L[i]
+            k+=1
+            i+=1
+        while j<len(R)
+            A[k]= R[j]
+            k+=1
+            j+=1
